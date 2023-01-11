b245f714c7c2fce8eaec34bfc9d36a4d812aab98
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-11 17:34:29 +0100

    MessageDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ function App() {
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gay-y-12 overflow-scroll">
-      <h2 className="text-4xl text-yellow-300">hello world</h2>
+      {
+        users.map((user, i) => <UserDisplay user={user} key={i} />)
+      }
     </div>
   );
 }
---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import type { Message } from "../types";
+
+type Props = {
+    message: Message
+    index: Number
+}
+
+function MessageDisplay({ message }: Props) {
+    return (
+        <div className="group mb-2 shrink-0 rounded-lg flex items-center drop-shadow-md bg-zinc-700 w-48 h-20 relative">
+            <p className="text-sm text-gray-200 font-bold px-4">
+                {message.body}
+            </p>
+        </div>
+    )
+}
+
+export default MessageDisplay
