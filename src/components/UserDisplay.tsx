903ddfbd70e82244efe405c40762327e24c1d416
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-11 17:40:36 +0100

    putting both toguether

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { User } from "../types";
+import MessageDisplay from './MessageDisplay';
 
 type Props = {
     user: User
@@ -11,7 +12,11 @@ function UserDisplay({ user }: Props) {
             <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
                 <p className="text-xl text-gray-200 font-bold">{user.name}</p>
             </div>
+            <div>
+                {user.messages.map((message, i) => <MessageDisplay key={i} index={i} message={message} />)}
         </div>
+        </div>
+
     )
 }
 
