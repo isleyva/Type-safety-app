9387da3d6ffa021fbb5668b2f7235df4a204b162
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-11 11:50:39 +0100

    debuging and type definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import type { User } from './types'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      hello world
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gay-y-12 overflow-scroll">
+      <h2 className="text-4xl text-yellow-300">hello world</h2>
     </div>
   )
 }
