02b2e7419cda8c3e0209c637f8fa0c4b07804595
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-01-11 17:24:27 +0100

    UserDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import type { User } from './types'
+import UserDisplay from "./components/UserDisplay";
+import type { User } from "./types";
 
 function App() {
+  const users: User[] = [
+    {
+      name: "bob chen",
+      messages: [
+        { body: "hello" },
+        {
+          body: "whats up",
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gay-y-12 overflow-scroll">
       <h2 className="text-4xl text-yellow-300">hello world</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
